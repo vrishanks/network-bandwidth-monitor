3bc0c08f08a0116435ca150282659442320a80f7
vrishanks <99161451+vrishanks@users.noreply.github.com>
2024-04-12 12:03:56 +0000

    #2: Add dataset.py file to generate data

---
+++ dataset.py
@@ -0,0 +1,16 @@
+from scapy.all import Ether, IP, TCP, wrpcap
+import random
+
+def generate_packet_capture_data(file_path):
+    packets = []
+    for i in range(800):
+        src_ip = "10.0." + ".".join(str(random.randint(0, 255)) for _ in range(2))
+        dst_ip = "192.168." + ".".join(str(random.randint(0, 255)) for _ in range(2))
+        packet_size = random.randint(64, 2000)
+        
+        packet = Ether() / IP(src=src_ip, dst=dst_ip) / TCP() / ("X" * packet_size)
+        packets.append(packet)
+    
+    wrpcap(file_path, packets)
+
+generate_packet_capture_data("new_synthetic_packet_capture.pcap")
