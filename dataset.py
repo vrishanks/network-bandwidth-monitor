2348f4780a6794338b50973ad04377fe20c2c12b
vrishanks <99161451+vrishanks@users.noreply.github.com>
2024-05-11 14:14:17 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,7 +4,7 @@ import random
 def generate_packet_capture_data(file_path):
     packets = []
     for i in range(800):
-        src_ip = "10.0." + ".".join(str(random.randint(0, 255)) for _ in range(2))
+        src_ip = "172.16." + ".".join(str(random.randint(0, 255)) for _ in range(2))
         dst_ip = "192.168." + ".".join(str(random.randint(0, 255)) for _ in range(2))
         packet_size = random.randint(64, 2000)
         
